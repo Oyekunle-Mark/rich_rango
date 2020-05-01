3be8a122279932e3f136b003a1bbca059393bac1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-01 20:58:40 +0100

    Added a background and network image

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('I am Rich'),
           backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Image(
+          image: NetworkImage(
+              'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
         ),
       ),
     ),
