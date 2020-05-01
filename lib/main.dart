3a9598d0fc0c8d3818eff7e661fd1b938e315d15
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-01 20:31:25 +0100

    Even more formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Center(
-          child: Text('Hello, Flutter!'),
-        ),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello, Flutter!'),
       ),
-    );
+    ),
+  );
+}
